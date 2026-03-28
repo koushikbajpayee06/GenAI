d5538a6a7abfe6eb066ad56f7c03e4bab1d0a663
Koushik <Koushikbajpayee06@gmail.com>
2026-03-28 21:30:50 +0530

    added to parameter temperature and max_completion_token in the model

--- 2.ChatModels/1_chatmodel_openai.py
+++ 2.ChatModels/1_chatmodel_openai.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI(model='gpt-4')
-result = model.invoke("what is the capital of india?")
+model = ChatOpenAI(model='gpt-4',temperature=1.8, max_completion_tokens=10)
+result = model.invoke("suggest me 5 IT fields where hiring is high in india")
 print(result.content)
